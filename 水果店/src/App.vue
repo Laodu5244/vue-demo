4847f515143f0584bdf495360d9699a706a9fef2
Laodu5244 <99964861+Laodu5244@users.noreply.github.com>
2022-06-06 11:57:15 +0800

    Add files via upload

---
+++ 水果店/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <table>
+      <h3>欢迎光临Vue水果店</h3>
+      <tr>
+        <th>苹果: {{ price }} 元/斤, 折扣: {{ dis * 10 }} 折</th>
+      </tr>
+      <tr>
+        <td>
+          买几斤:
+          <input type="number" placeholder="斤数" v-model.number="count" />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <button @click="cel">结账</button>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <span>总价:{{ allPrice }} 元</span>
+          <span>折后价:{{ disPrice }} 元</span>
+          <span>节省:{{ savePrice }} 元</span>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      price: 5, // 单价
+      dis: 0.7, // 折扣
+      count: 0, // 斤数
+      allPrice: 0, // 总价
+      disPrice: 0, // 折后价
+      savePrice: 0, // 节省
+    };
+  },
+  methods: {
+    cel() {
+      this.allPrice = this.price * this.count;
+      this.disPrice = this.allPrice * this.dis;
+      this.savePrice = this.allPrice - this.disPrice;
+    },
+  },
+};
+</script>
+
+<style>
+table {
+  width: 600px;
+  text-align: center;
+  margin: 0 auto;
+  border: 1px solid #444;
+}
+span {
+  margin: 10px;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
---
+++ 筛选列表/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>请选择喜欢的专栏</p>
+    <div class="item" v-for="(t, i) in titleArr" :key="i">
+      <input type="checkbox" :value="t" v-model="sel" /><span>{{ t }}</span>
+    </div>
+    <ul>
+      <li v-for="(str, i) in sel" :key="i">{{ str }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titleArr: ["科幻", "喜剧", "动漫", "冒险", "军事", "娱乐", "奇闻"],
+      sel: [],
+    };
+  },
+};
+</script>
+
+<style>
+.item {
+  margin: 5px;
+  display: inline-block;
+}
+</style>
+
