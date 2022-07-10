b99f8bb3e6f65c2823be0841e1dbfd6212554e74
Laodu5244 <99964861+Laodu5244@users.noreply.github.com>
2022-07-10 19:02:07 +0800

    Add files via upload

---
+++ 水果购物车/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <Carts></Carts>
+  </div>
+</template>
+
+<script>
+import Carts from "./components/Carts.vue";
+export default {
+  components: { Carts },
+};
+</script>
+
+
+
