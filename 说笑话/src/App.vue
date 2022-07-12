75685e92c1b64cfc0ee2c3ffa2fec4311fa40d56
Laodu5244 <99964861+Laodu5244@users.noreply.github.com>
2022-07-12 14:50:06 +0800

    Add files via upload

---
+++ 说笑话/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="getJoke">获取笑话</button>
+    <p style="width:50%">{{ joke }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      joke: '很好笑的笑话'
+    }
+  },
+  methods: {
+    getJoke() {
+      axios.get('https://autumnfish.cn/api/joke')
+        .then((res) => {
+          this.joke = res.data
+        })
+    }
+  }
+}
+</script>
